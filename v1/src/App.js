4224aba669f45fc8d88e1928d9afa51ffb5f362a
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-21 16:55:16 +0530

    dfsfgdhfa

--- v1/src/App.js
+++ v1/src/App.js
@@ -2,9 +2,21 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {lat : null}
+
+    console.log("dahgad");
+    this.setState({lat : "adjkshdak"});
+  }
+
+  componentDidMount(){
+    console.log("component is mounted");
+  }
   render(){
     console.log("Hi from the component");
-    return <div>Hello from class component</div>
+    return <div>Latitude : {this.state.lat}</div>
   }
 }
 
