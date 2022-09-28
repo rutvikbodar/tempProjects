1ba45e3f8662baac950a365cae81c9cc8a3d2546
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-28 18:06:31 +0530

    search backend

--- videos/src/Searchbar.js
+++ videos/src/Searchbar.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 export default class Searchbar extends React.Component{
+
+    state = {term : ""}
+
+    onChangeHandler = (value) =>{
+        this.setState({term : value.target.value});
+        console.log(this.state.term);
+    }
+
     render(){
         return(
-            <div>
-                Search Bar
+            <div className="search-bar ui segment">
+                <form action="" className="ui form">
+                    <div className="field">
+                        <label htmlFor="">
+                            Video Search
+                        </label>
+                        <input type={"text"} value={this.state.term} onChange={this.onChangeHandler}>
+                        </input>
+                    </div>
+                </form>
             </div>
         )
     }
