d70737c9b762f623f6245b5a426fda40d1cd4138
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-28 17:49:50 +0530

    searchbar init

---
+++ videos/src/Searchbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Searchbar extends React.Component{
+    render(){
+        return(
+            <div>
+                Search Bar
+            </div>
+        )
+    }
+}
