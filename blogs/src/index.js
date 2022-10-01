30c361ff24ef37476f9ee7131f8932d0e106bf62
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-10-01 19:10:02 +0530

    blogs redux partial setup

--- blogs/src/index.js
+++ blogs/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
 );
