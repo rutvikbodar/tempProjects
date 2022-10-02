f8497105250ea75492c7bec1347a84c4a7a8c495
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-10-02 17:38:25 +0530

    sadhjasndasd

--- blogs/src/actions/index.js
+++ blogs/src/actions/index.js
@@ -1,11 +1,14 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+import _ from 'lodash';
  
 export const fetchPosts = () => async dispatch =>{
         const Results = await jsonPlaceHolder.get('/posts')
         dispatch({type : 'FETCH_POSTS', payload: Results})
     }
 
-export const fetchUser = (id) => async dispatch =>{
+export const fetchUser = id => dispatch =>_fetchUser(id,dispatch);
+
+const _fetchUser =_.memoize(async(id, dispatch)=>{
     const Results = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({type : "FETCH_USER",  payload : Results})
-}
+})
