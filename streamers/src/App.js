cf9cfbd69ee0d1b09a5bbcd5315e7d47cb479d45
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-10-03 14:25:55 +0530

    sbdsahdgasd

--- streamers/src/App.js
+++ streamers/src/App.js
@@ -1,8 +1,25 @@
+import React from 'react'
+import {BrowserRouter , Route} from 'react-router-dom'
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="ui container">
+      <BrowserRouter>
+      <Header />
+      <Route path={"/"} exact component={StreamList} />
+      <Route path={"/stream/new"} exact component={StreamCreate} />
+      <Route path={"/stream/edit"} exact component={StreamEdit} />
+      <Route path={"/stream/delete"} exact component={StreamDelete} />
+      <Route path={"/stream/show"} exact component={StreamShow} />
+      </BrowserRouter>
     </div>
   );
 }
